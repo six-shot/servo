ef7f3b045d1805e29b0f0f9666372e97771e2ad7
six-shot <okhuomonajayi54@gmail.com>
2024-03-20 08:59:55 +0000

    fix clippy problems in task_queue

--- components/script/stylesheet_loader.rs
+++ components/script/stylesheet_loader.rs
@@ -134,14 +134,14 @@ impl FetchResponseListener for StylesheetContext {
             });
 
             let data = if is_css {
-                mem::replace(&mut self.data, vec![])
+                std::mem::take(&mut self.data)
             } else {
                 vec![]
             };
 
             // TODO: Get the actual value. http://dev.w3.org/csswg/css-syntax/#environment-encoding
             let environment_encoding = UTF_8;
-            let protocol_encoding_label = metadata.charset.as_ref().map(|s| &**s);
+            let protocol_encoding_label = metadata.charset.as_deref();
             let final_url = metadata.final_url;
 
             let win = window_from_node(&*elem);
@@ -179,7 +179,7 @@ impl FetchResponseListener for StylesheetContext {
                 },
                 StylesheetContextSource::Import(ref stylesheet) => {
                     Stylesheet::update_from_bytes(
-                        &stylesheet,
+                        stylesheet,
                         &data,
                         protocol_encoding_label,
                         Some(environment_encoding),
@@ -277,7 +277,7 @@ impl<'a> StylesheetLoader<'a> {
             .downcast::<HTMLLinkElement>()
             .map(HTMLLinkElement::get_request_generation_id);
         let context = ::std::sync::Arc::new(Mutex::new(StylesheetContext {
-            elem: Trusted::new(&*self.elem),
+            elem: Trusted::new(self.elem),
             source: source,
             url: url.clone(),
             metadata: None,
